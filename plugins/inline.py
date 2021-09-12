489431c793ce46e36740a3a80e63c4d0d229f513
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2021-09-12 18:53:31 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Support Group', url='https://t.me/miuiicons_discussion')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
